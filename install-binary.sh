6f0dbfaf5c80816819f8a960d2c0be2f204bebae
nikhilsbhat <nikhilsbhat93@gmail.com>
2021-09-02 20:17:25 +0530

    Refactor install script

--- install-binary.sh
+++ install-binary.sh
@@ -1,15 +1,14 @@
 #! /bin/bash -e
 
-function download() {
+function download_plugin() {
   osName=$(uname -s)
-  DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/nikhilsbhat/helm-images/releases/latest" | grep -o "browser_download_url.*\_${osName}_x86_64.tar.gz")
+  DOWNLOAD_URL=$(curl --silent "https://api.github.com/repos/nikhilsbhat/helm-images/releases/latest" | grep -o "browser_download_url.*\_${osName}_x86_64.zip")
 
   DOWNLOAD_URL=${DOWNLOAD_URL//\"/}
   DOWNLOAD_URL=${DOWNLOAD_URL/browser_download_url: /}
 
-  echo "download url: $DOWNLOAD_URL"
   OUTPUT_BASENAME=helm-images
-  OUTPUT_BASENAME_WITH_POSTFIX=$HELM_PLUGIN_DIR/$OUTPUT_BASENAME.tar.gz
+  OUTPUT_BASENAME_WITH_POSTFIX="$HELM_PLUGIN_DIR$OUTPUT_BASENAME.zip"
 
   if [ -z "$DOWNLOAD_URL" ]; then
     echo "Unsupported OS / architecture: ${osName}"
@@ -28,22 +27,28 @@ function download() {
 }
 
 function install_plugin() {
-  HELM_TMP="/tmp/$PROJECT_NAME"
-  mkdir -p "$HELM_TMP"
-  tar xf "$PLUGIN_TMP_FILE" -C "$HELM_TMP"
-  HELM_TMP_BIN="$HELM_TMP/diff/bin/diff"
+  local HELM_PLUGIN_ARTIFACT_PATH=$1
+  #  echo "HELM_PLUGIN_ARTIFACT_PATH: $HELM_PLUGIN_ARTIFACT_PATH"
   echo "Preparing to install into ${HELM_PLUGIN_DIR}"
-  mkdir -p "$HELM_PLUGIN_DIR/bin"
-  cp "$HELM_TMP_BIN" "$HELM_PLUGIN_DIR/bin"
-
+  mkdir -p "$HELM_PLUGIN_DIR/helm-images/bin"
+  tar -xvf "$HELM_PLUGIN_ARTIFACT_PATH" -C "$HELM_PLUGIN_DIR"helm-images
+  mv "$HELM_PLUGIN_DIR"helm-images/helm-images "$HELM_PLUGIN_DIR"helm-images/bin
+  rm -rf "$HELM_PLUGIN_ARTIFACT_PATH"
 }
 
 function install() {
   echo "Installing helm-images..."
 
-  local artifact_path=$(download)
-  echo "$artifact_path"
-  #  rm -rf bin && mkdir bin && tar -xvf $OUTPUT_BASENAME_WITH_POSTFIX -C bin >/dev/null && rm -f $OUTPUT_BASENAME_WITH_POSTFIX
+  local ARTIFACT_PATH=$(download_plugin)
+  set +e
+  install_plugin "$ARTIFACT_PATH"
+  local INSTALL_PLUGIN_STAT=$?
+  set -e
+
+  if [ ! $INSTALL_PLUGIN_STAT -eq 0 ]; then
+    echo "installing helm plugin helm-images failed with error code: $INSTALL_PLUGIN_STAT"
+    exit 1
+  fi
 
   echo "helm-images is installed."
   echo
